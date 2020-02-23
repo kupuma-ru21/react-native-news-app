7577703b76854cda2de104a63a1cebe72e5d9d5d
kupuma-rur21 <kupumaru21@gmail.com>
2020-02-23 17:09:50 +0900

    feat: ListItemコンポーネントを作成

---
+++ components/ListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
+
+const ListItem = ({ imageUrl, title, author, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.itemContainer} onPress={onPress}>
+      <View style={styles.leftContainer}>
+        {!!imageUrl && (
+          <Image
+            source={{ uri: imageUrl }}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+      </View>
+
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          {title}
+        </Text>
+
+        <Text style={styles.subText}>{author}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ListItem;
